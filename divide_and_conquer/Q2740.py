f8ef0828de6a18228433391452bdb1ae1e23955a
silverz <liisa9601@naver.com>
2021-09-02 20:08:46 +0900

    행렬 곱셈

---
+++ divide_and_conquer/Q2740.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(n)]
+m, k= map(int, input().split())
+B= [list(map(int, input().split())) for _ in range(m)]
+result = [[0 for _ in range(k)]for _ in range(n)]
+
+for i in range(n):
+   for j in range(k):
+       for l in range(m):
+           result[i][j]+=A[i][l]*B[l][j]
+
+                
+                
+for i in range(n):
+    for j in range(k):
+        print(result[i][j], end=" ")
+    print()
